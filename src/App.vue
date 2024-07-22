90d5682cecb751e476319c75e2b42f8070d2a5d6
Kwon <kwon@goodrammers.com>
2024-07-22 14:30:02 +0900

    초안

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,55 @@
 <template>
-    <div class="g-flex-center" style="min-height: 100vh">
-        <Box :border-width="borderWidth">
-            <div class="g-flex-center" style="height: 100%">Hello</div>
+    <div class="g-flex-center" style="min-height: 100vh; flex-direction: column">
+        <Box
+            :border-width="borderWidth"
+            :height="height"
+            :width="width"
+            :duration="duration"
+            :pause="pause"
+        >
+            <div class="g-flex-center content">
+                <img
+                    width="90%"
+                    src="https://media.istockphoto.com/id/1496970435/ko/%EC%82%AC%EC%A7%84/%EC%B6%94%EC%83%81%EC%A0%81%EC%9D%B8-vr-%ED%99%98%EA%B2%BD.jpg?s=2048x2048&w=is&k=20&c=J4iz0HbChW_BohXVsXz7VhIJkNKZaJ8mkf5xPoOro3o="
+                />
+            </div>
         </Box>
+
+        <div style="margin-top: 20px; color: white" class="g-flex-center">
+            <div class="my-button" @click="pause = !pause">
+                <span style="font-size: 40px; color: white" :class="pauseClassName"></span>
+            </div>
+            <div style="margin-left: 30px">속도</div>
+            <input style="margin-left: 10px" v-model="duration" type="range" min="2" max="20" />
+            <div style="margin-left: 10px; width: 40px">{{ duration }}</div>
+        </div>
     </div>
 </template>
 
 <script setup lang="ts">
 import Box from '@/components/Box.vue'
-import { ref } from 'vue'
+import { computed, ref } from 'vue'
 const borderWidth = ref('10px')
+const width = ref('500px')
+const height = ref('350px')
+const duration = ref(10)
+const pause = ref(false)
+const pauseClassName = computed(() =>
+    pause.value ? 'mdi mdi-play-circle-outline' : 'mdi mdi-pause-circle-outline'
+)
 </script>
 
-<style scoped></style>
+<style lang="scss">
+body {
+    background-color: black;
+}
+.content {
+    background-color: black;
+    width: 100%;
+    height: 100%;
+    border-radius: 20px;
+}
+.my-button {
+    cursor: pointer;
+}
+</style>
--- src/components/Box.vue
+++ src/components/Box.vue
@@ -1,7 +1,7 @@
 <template>
     <div class="box-container g-relative">
         <div class="box-border-effect"></div>
-        <div class="g-abs-full" style="padding: 20px; z-index: 2">
+        <div class="g-abs-full" style="padding: 5px; z-index: 2">
             <slot></slot>
         </div>
     </div>
@@ -10,11 +10,25 @@
 <script setup lang="ts">
 import { computed, toRefs } from 'vue'
 
-const p = defineProps<{ width?: string; height?: string; borderWidth?: string }>()
-const { width: _width, height: _height, borderWidth: _borderWidth } = toRefs(p)
+const p = defineProps<{
+    width?: string
+    height?: string
+    borderWidth?: string
+    duration?: number
+    pause?: boolean
+}>()
+const {
+    width: _width,
+    height: _height,
+    borderWidth: _borderWidth,
+    duration: _duration,
+    pause,
+} = toRefs(p)
 const width = computed(() => (_width.value ? _width.value : '200px'))
 const height = computed(() => (_height.value ? _height.value : '200px'))
 const borderWidth = computed(() => (_borderWidth.value ? _borderWidth.value : '6px'))
+const duration = computed(() => (_duration.value ? _duration.value + 's' : '4s'))
+const playOrStop = computed(() => (pause.value ? 'paused' : 'running'))
 </script>
 
 <style lang="scss" scoped>
@@ -31,17 +45,22 @@ const borderWidth = computed(() => (_borderWidth.value ? _borderWidth.value : '6
         --border-effect-deg: 360deg;
     }
 }
-.box-border-effect {
-    position: relative;
+.box-container {
     width: v-bind(width);
     height: v-bind(height);
+}
+.box-border-effect {
+    position: relative;
+    width: 100%;
+    height: 100%;
     background: repeating-conic-gradient(
         from var(--border-effect-deg),
         #ff2770 0% 5%,
         transparent 5% 144deg,
         #ff2770 180deg
     );
-    animation: animate 4s linear infinite;
+    animation: animate v-bind(duration) linear infinite;
+    animation-play-state: v-bind(playOrStop);
     border-radius: 20px;
     &:before {
         content: '';
@@ -56,8 +75,10 @@ const borderWidth = computed(() => (_borderWidth.value ? _borderWidth.value : '6
             transparent 40%,
             #45f3ff 50%
         );
-        animation: animate 4s linear infinite;
-        animation-delay: -1s;
+
+        animation: animate v-bind(duration) linear infinite;
+        animation-play-state: v-bind(playOrStop);
+        animation-delay: calc(-1 * v-bind(duration) / 4);
         border-radius: 20px;
     }
     &:after {
@@ -66,7 +87,7 @@ const borderWidth = computed(() => (_borderWidth.value ? _borderWidth.value : '6
         background: white;
         z-index: 1;
         inset: 10px;
-        border-radius: 16px;
+        border-radius: 20px;
     }
 }
 </style>
